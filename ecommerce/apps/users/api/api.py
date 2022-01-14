f46c79fbd0c70242798b12667ed5c09e99fe604d
camilo <quinterocamilo771@gmail.com>
2022-01-13 21:04:09 -0500

    insercion de la funcion update como vista

--- ecommerce/apps/users/api/api.py
+++ ecommerce/apps/users/api/api.py
@@ -6,17 +6,18 @@ from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST']) # Utilizando funciones para crear la vista de la API
 def user_api_view(request):
-    #    Utilizando funciones para crear la vista de la API
-    #   metodo GET para enviar solicitud y que devuelva la data
+
+    # METODO GET PARA ENVIAR SOLICITUD DE TODOS LOS ELEMENTOS
     if request.method == 'GET':
         user = User.objects.all()
         user_serializer = UserSerializer(user, many=True)
         return Response(user_serializer.data)
 
-    #   metodo POST para enviar data y crear en la base de datos instancias
+    # METODO POST PARA ENVIAR DATA Y CREAR INSTANCIAS EN LA BD
     elif request.method == 'POST':
+        #   convierte los daton de json en python mediante el UserSerializer
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():  # Valida los datos enviados en el Json
             user_serializer.save()  # Guarda la data validada
@@ -28,21 +29,38 @@ def user_api_view(request):
         # Si los datos no son válidos muestra error
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def user_detail_api_view(request, pk=None):
-    #   metodo GET para solicitar una instancia específica de la data
+    # METODO GET PARA SOLICITAR UNA INSTANCIA ESPECIFICA
     if request.method == 'GET':
+        user = User.objects.filter(id=pk).first()
         # filtro por PK, el filter() no arroja errores, valida la instancia
         # si no encuentra el PK, no rompe el codigo, a diferencia del get()
-        user = User.objects.filter(id=pk).first()
         user_serializer = UserSerializer(user)
         return Response(user_serializer.data)
+
+    # METODO PUT PARA MODIFICAR UNA INSTANCIA ESPECIFICA
+    elif request.method == 'PUT':
+        user = User.objects.filter(id=pk).first()
+        user_serializer = UserSerializer(user, data=request.data)
+        # convierte los daton de json en python mediante el UserSerializer
+        # se pasan por parametros al UserSerializar la instancia (user) y la
+        # data (data = request.data) que se desea insertar
+
+        if user_serializer.is_valid():  # Valida los datos enviados en el Json
+            user_serializer.save()  # Guarda la data validada
+            print("usuario creado")
+            return Response(user_serializer.data,
+                            status=status.HTTP_202_ACCEPTED)
+        return Response(user_serializer.errors)
+        # Si los datos no son válidos muestra error
 
 
 class UserApiView(APIView):
     """ Utilizando Clases APIView"""
 
     def get(self, request):
+
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(UserSerializer.data)
