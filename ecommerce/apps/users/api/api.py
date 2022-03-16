945cbb132ba783c13f26065bb22ab20091ce015d
camilo <quinterocamilo771@gmail.com>
2022-03-16 11:24:31 -0500

    Serializers y vistas CRUD creadas
    Se crean metodos put, post, delete, update
    Se implementa serializer para listar y campos especificos
    Se implementa funcion para encriptar password de usuario

--- ecommerce/apps/users/api/api.py
+++ ecommerce/apps/users/api/api.py
@@ -3,64 +3,113 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer
+from apps.users.api.serializers import UserSerializer, SomeFieldsUserSerializer
 
 
-@api_view(['GET', 'POST']) # Utilizando funciones para crear la vista de la API
+@api_view(['GET', 'POST'])
 def user_api_view(request):
+    """función para solicitar y crear elementos en la BD."""
 
-    # METODO GET PARA ENVIAR SOLICITUD DE TODOS LOS ELEMENTOS
+    """METODO GET PARA ENVIAR SOLICITUD DE LISTA DE TODOS LOS ELEMENTOS"""
     if request.method == 'GET':
+        # queryset
         user = User.objects.all()
         user_serializer = UserSerializer(user, many=True)
-        return Response(user_serializer.data)
+        
+        return Response(user_serializer.data, status=status.HTTP_200_OK)
+        # en el response si no se especifica, devuelve por default 200ok
 
-    # METODO POST PARA ENVIAR DATA Y CREAR INSTANCIAS EN LA BD
     elif request.method == 'POST':
-        #   convierte los daton de json en python mediante el UserSerializer
+        """METODO POST PARA ENVIAR DATA Y CREAR INSTANCIAS EN LA BD
+        convierte los daton de json en python mediante el UserSerializer"""
+
         user_serializer = UserSerializer(data=request.data)
+
+        # validacion de los datos
         if user_serializer.is_valid():  # Valida los datos enviados en el Json
             user_serializer.save()  # Guarda la data validada
             print("usuario creado")
-            return Response(user_serializer.data,
-                            # Responde la data creada
+            return Response({'message': 'Usuario Creado correctamente!'},
                             status=status.HTTP_201_CREATED)
-        return Response(user_serializer.errors)
         # Si los datos no son válidos muestra error
+        return Response(user_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def user_detail_api_view(request, pk=None):
-    # METODO GET PARA SOLICITAR UNA INSTANCIA ESPECIFICA
+    """funcion para solicitar, actualizar y borrar mediante filtro inicial por
+    PK UNA instancia de la BD. Se utiliza el filter() en la consulta inicial
+    porque no arroja errores en caso de no encontrarse los datos (PK),
+    no rompe el codigo, a diferencia del get()."""
+    user = User.objects.filter(id=pk).first()
+
+    if user:
+        """METODO GET PARA SOLICITAR UNA INSTANCIA ESPECIFICA"""
+        if request.method == 'GET':
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data)
+
+        elif request.method == 'PUT':
+            """METODO PUT PARA MODIFICAR UNA INSTANCIA ESPECIFICA"""
+            user_serializer = UserSerializer(user, data=request.data)
+            """user_serializer convierte los daton de json en python mediante 
+            el UserSerializer.py y se pasan por parametros al UserSerializar la 
+            instancia (user) y la data (data = request.data) que se desea 
+            insertar"""
+            if user_serializer.is_valid():
+                """Valida los datos enviados en el Json"""
+                user_serializer.save()
+                """Guarda la data validada"""
+                print("usuario creado")
+                return Response(user_serializer.data,
+                                status=status.HTTP_202_ACCEPTED)
+            return Response(user_serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+            # Si los datos no son válidos muestra error
+
+        elif request.method == 'DELETE':
+            """METODO DELETE PARA ELIMINAR UNA INSTANCIA ESPECIFICA"""
+            user.delete()
+            return Response({'message': 'Usuario ELiminado correctamente!'},
+                            status=status.HTTP_200_OK)
+    return Response({'message': 'No se ha encontrado usuario con estos datos'},
+                    status=status.HTTP_200_OK)
+
+
+@api_view(['GET', 'POST'])
+def user_some_fields_api_view(request):
+    """función para solicitar y listar elementos especificos de la BD."""
+
+    """METODO GET PARA ENVIAR SOLICITUD DE LISTA DE ELEMENTOS ESPECIFICOS"""
     if request.method == 'GET':
-        user = User.objects.filter(id=pk).first()
-        # filtro por PK, el filter() no arroja errores, valida la instancia
-        # si no encuentra el PK, no rompe el codigo, a diferencia del get()
-        user_serializer = UserSerializer(user)
-        return Response(user_serializer.data)
+        # queryset
+        user = User.objects.all().values('id', 'username', 'email', 'password')
+        user_serializer = SomeFieldsUserSerializer(user, many=True)
 
-    # METODO PUT PARA MODIFICAR UNA INSTANCIA ESPECIFICA
-    elif request.method == 'PUT':
-        user = User.objects.filter(id=pk).first()
-        user_serializer = UserSerializer(user, data=request.data)
-        # convierte los daton de json en python mediante el UserSerializer
-        # se pasan por parametros al UserSerializar la instancia (user) y la
-        # data (data = request.data) que se desea insertar
+        return Response(user_serializer.data, status=status.HTTP_200_OK)
+        # en el response si no se especifica, devuelve por default 200ok
+    elif request.method == 'POST':
+        """METODO POST PARA ENVIAR DATA Y CREAR INSTANCIAS EN LA BD
+        convierte los daton de json en python mediante el UserSerializer"""
 
+        user_serializer = SomeFieldsUserSerializer(data=request.data)
+
+        # validacion de los datos
         if user_serializer.is_valid():  # Valida los datos enviados en el Json
             user_serializer.save()  # Guarda la data validada
             print("usuario creado")
-            return Response(user_serializer.data,
-                            status=status.HTTP_202_ACCEPTED)
-        return Response(user_serializer.errors)
+            return Response({'message': 'Usuario Creado correctamente!'},
+                            status=status.HTTP_201_CREATED)
         # Si los datos no son válidos muestra error
+        return Response(user_serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserApiView(APIView):
     """ Utilizando Clases APIView"""
 
     def get(self, request):
-
         users = User.objects.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(UserSerializer.data)
--- ecommerce/apps/users/api/serializers.py
+++ ecommerce/apps/users/api/serializers.py
@@ -6,4 +6,60 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
-        
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        update_user = super().update(instance, validated_data)
+        update_user.set_password(validated_data['password'])
+        update_user.save()
+        return update_user
+
+
+class SomeFieldsUserSerializer(serializers.ModelSerializer):
+    """ Clase para crear metodo que permita listar en el serializar campos
+    especificos del modelo, al eliminar y actualizar la clase si utiliza
+    todos los campos del modelo"""
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        return {
+            'id': instance['id'],
+            'username': instance['username'],
+            'email': instance['email'],
+            'password': instance['password']
+        }
+
+
+class TestUserSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
+
+    def validated_name(self, value):
+        if 'camilo' in value:
+            raise serializers.ValidationError('Error')
+        return value
+
+    def validated_email(self, value):
+        if value == "":
+            raise serializers.ValidationError('Tiene que indicar correo')
+        return value
+
+    def validate(self, data):
+        if data['name'] in data['email']:
+             raise serializers.ValidationError('El email contiene el nombre')
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
